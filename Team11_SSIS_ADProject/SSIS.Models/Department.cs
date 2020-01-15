28d4b9ee465159e33fd0c69c9f43b9a3927b0879
Hein Htet Zaw <hhz18@icloud.com>
2020-01-15 16:45:20 +0800

    added required annotation

--- Team11_SSIS_ADProject/SSIS.Models/Department.cs
+++ Team11_SSIS_ADProject/SSIS.Models/Department.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,12 +8,19 @@ namespace Team11_SSIS_ADProject.SSIS.Models
 {
     public class Department : BaseEntity
     {
+        [Required]
         public string DepartmentCode { get; set; }
+        [Required]
         public string DepartmentName { get; set; }
+        [Required]
         public string DepartmentContactName { get; set; }
+        [Required]
         public string DepartmentPhone { get; set; }
+        [Required]
         public string DepartmentFax { get; set; }
+        [Required]
         public string DepartmentHeadName { get; set; }
+        [Required]
         public string DepartmentRepresentative { get; set; }
     }
 }
--- Team11_SSIS_ADProject/SSIS.Models/Supplier.cs
+++ Team11_SSIS_ADProject/SSIS.Models/Supplier.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,12 +8,19 @@ namespace Team11_SSIS_ADProject.SSIS.Models
 {
     public class Supplier : BaseEntity
     {
+        [Required]
         public string SupplierCode { get; set; }
+        [Required]
         public string SupplierName { get; set; }
+        [Required]
         public string SupplierContactName { get; set; }
+        [Required]
         public string SupplierPhone { get; set; }
+        [Required]
         public string SupplierFax { get; set; }
+        [Required]
         public string SupplierAddress { get; set; }
+        [Required]
         public string SupplierGSTNumber { get; set; }
     }
 }
