bd8e8279e62591333eb37067cacf29ef3343a429
Hein Htet Zaw <hhz18@icloud.com>
2020-01-16 09:49:16 +0800

    added supplier view

--- Team11_SSIS_ADProject/SSIS.Models/Supplier.cs
+++ Team11_SSIS_ADProject/SSIS.Models/Supplier.cs
@@ -8,19 +8,12 @@ namespace Team11_SSIS_ADProject.SSIS.Models
 {
     public class Supplier : BaseEntity
     {
-        [Required]
         public string SupplierCode { get; set; }
-        [Required]
         public string SupplierName { get; set; }
-        [Required]
         public string SupplierContactName { get; set; }
-        [Required]
         public string SupplierPhone { get; set; }
-        [Required]
         public string SupplierFax { get; set; }
-        [Required]
         public string SupplierAddress { get; set; }
-        [Required]
         public string SupplierGSTNumber { get; set; }
     }
 }
