d54aeee2023c2d8e97095ca4c4bd6bb6ea14ad38
Hein Htet Zaw <hhz18@icloud.com>
2020-01-17 10:00:05 +0800

    added supplier crud

--- Team11_SSIS_ADProject/Controllers/SupplierController.cs
+++ Team11_SSIS_ADProject/Controllers/SupplierController.cs
@@ -3,15 +3,76 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Team11_SSIS_ADProject.SSIS.Models;
+using Team11_SSIS_ADProject.SSIS.Service;
+using Team11_SSIS_ADProject.SSIS.ViewModels;
 
 namespace Team11_SSIS_ADProject.Controllers
 {
     public class SupplierController : Controller
     {
+        SupplierService supplierService;
+        Supplier supplier;
+        public SupplierController()
+        {
+            this.supplierService = new SupplierService();
+        }
+        public ActionResult Create()
+        {
+            var supplier = new SupplierViewModel();
+            return View("SupplierForm", supplier);
+        }
         // GET: Supplier
         public ActionResult Index()
         {
-            return View();
+            Supplier s = new Supplier();
+            var viewModel = new SupplierViewModel()
+            {
+
+                SupplierCode = s.SupplierCode,
+                SupplierName = s.SupplierName,
+                SupplierPhone = s.SupplierPhone,
+                SupplierAddress = s.SupplierAddress,
+                SupplierFax = s.SupplierFax,
+                SupplierGSTNumber = s.SupplierGSTNumber,
+                SupplierContactName = s.SupplierContactName,
+
+                Suppliers = supplierService.GetAll()
+            };
+
+            return View(viewModel);
+        }
+        [HttpPost]
+        public ActionResult Save(Supplier supplier)
+        {
+            supplierService.Save(supplier);
+
+            return RedirectToAction("Index", "Supplier");         
+        }
+        public ActionResult Edit(string id)
+        {
+            var s = supplierService.Get(id);
+            var supplier = new SupplierViewModel()
+            {
+                SupplierCode = s.SupplierCode,
+                SupplierName = s.SupplierName,
+                SupplierPhone = s.SupplierPhone,
+                SupplierAddress = s.SupplierAddress,
+                SupplierFax = s.SupplierFax,
+                SupplierGSTNumber = s.SupplierGSTNumber,
+                SupplierContactName = s.SupplierContactName
+            };
+
+            return View("SupplierForm", supplier);
+        }
+
+        public ActionResult Delete(string id)
+        {
+            var supplier = supplierService.Get(id);
+
+            supplierService.Delete(id);
+
+            return RedirectToAction("Index", "Supplier");
         }
     }
 }
---
+++ Team11_SSIS_ADProject/SSIS.Service/SupplierService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Team11_SSIS_ADProject.SSIS.Contracts;
+using Team11_SSIS_ADProject.SSIS.Models;
+using Team11_SSIS_ADProject.SSIS.Repository;
+
+namespace Team11_SSIS_ADProject.SSIS.Service
+{
+    public class SupplierService : ISupplierService
+    {
+        SupplierRepository supplierContext;
+        public SupplierService()
+        {
+            this.supplierContext = new SupplierRepository();
+        }
+
+        public void Delete(string Id)
+        {
+            var supplier = supplierContext.Get(Id);
+            supplierContext.Remove(supplier);
+            supplierContext.Commit();
+        }
+
+        public Supplier Get(string id)
+        {
+            return supplierContext.Get(id);
+        }
+
+        public IEnumerable<Supplier> GetAll()
+        {
+            return supplierContext.GetAll();
+        }
+
+        public void Save(Supplier supplier)
+        {
+            Supplier s = supplierContext.Get(supplier.Id);
+            if(s == null)
+            {
+                supplierContext.Add(supplier);
+            }
+            else
+            {
+                s.SupplierName = supplier.SupplierName;
+                s.SupplierPhone = supplier.SupplierPhone;
+                s.SupplierGSTNumber = supplier.SupplierGSTNumber;
+                s.SupplierFax = supplier.SupplierFax;
+                s.SupplierContactName = supplier.SupplierContactName;
+                s.SupplierAddress = supplier.SupplierAddress;
+                s.SupplierCode = supplier.SupplierCode;
+            }
+            supplierContext.Commit();
+        }
+    }
+}
