563303b7413aa8cbba4ce5900f579a6e2d7800e1
basmoirak <daniel_yn@yahoo.com.sg>
2020-01-19 23:15:24 +0800

    Resolved Migration Conflicts.

---
+++ Team11_SSIS_ADProject/Migrations/202001191506404_resolve_conflcits.cs
@@ -0,0 +1,18 @@
+﻿namespace Team11_SSIS_ADProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class resolve_conflcits : DbMigration
+    {
+        public override void Up()
+        {
+
+        }
+        
+        public override void Down()
+        {
+
+        }
+    }
+}
