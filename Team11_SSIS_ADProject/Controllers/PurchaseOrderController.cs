68b77c2a2e8cbca4807aa6a7612b77bb8541bf36
Sai Leng <sai.caesar.leng16@gmail.com>
2020-02-05 22:06:38 +0800

    implemented PurchaseOrder controller,ViewModel and View(Index,SupplierList)

--- Team11_SSIS_ADProject/Controllers/PurchaseOrderController.cs
+++ Team11_SSIS_ADProject/Controllers/PurchaseOrderController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Team11_SSIS_ADProject.SSIS.Contracts;
 using Team11_SSIS_ADProject.SSIS.Contracts.Services;
+using Team11_SSIS_ADProject.SSIS.ViewModels;
 
 namespace Team11_SSIS_ADProject.Controllers
 {
@@ -11,15 +13,38 @@ namespace Team11_SSIS_ADProject.Controllers
     {
         IPurchaseOrderService purchaseOrderService;
         IItemPurchaseOrderService itemPurchaseOrderService;
-        public PurchaseOrderController(IPurchaseOrderService purchaseOrderService, IItemPurchaseOrderService itemPurchaseOrderService)
+        ISupplierService supplierService;
+        IItemSupplierService itemSupplierService;
+        public PurchaseOrderController(IPurchaseOrderService purchaseOrderService, IItemPurchaseOrderService itemPurchaseOrderService, ISupplierService supplierService)
         {
             this.purchaseOrderService = purchaseOrderService;
             this.itemPurchaseOrderService = itemPurchaseOrderService;
+            this.supplierService = supplierService;
         }
         // GET: PurchaseOrder
         public ActionResult Index()
         {
-            return View();
+            var purchaseOrderViewModel = new PurchaseOrderViewModel()
+            {              
+                PurchaseOrders = purchaseOrderService.GetAll().OrderByDescending(r => r.createdDateTime)
+            };
+            return View("Index", purchaseOrderViewModel);
+        }
+        public ActionResult SupplierList()
+        {
+            var supplierViewModel = new SupplierViewModel()
+            {
+                Suppliers = supplierService.GetAll()
+            };
+            return View("SupplierList", supplierViewModel);
+        }
+        public ActionResult PurchaseOrderForm(string id)
+        {
+            var purchaseOrderViewModel = new PurchaseOrderViewModel()
+            {
+                ItemSuppliers = itemSupplierService.GetItemsLowerThanReorderLevelBySupplier(id)
+            };
+            return View("PurchaseOrderForm", purchaseOrderViewModel);
         }
     }
 }
---
+++ Team11_SSIS_ADProject/SSIS.ViewModels/PurchaseOrderViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Team11_SSIS_ADProject.SSIS.Models;
+
+namespace Team11_SSIS_ADProject.SSIS.ViewModels
+{
+    public class PurchaseOrderViewModel : BaseEntity
+    {
+        [Required]
+        public string SupplierId { get; set; }
+        [Required]
+        public DateTime ExpectedDate { get; set; }    
+        public int Status { get; set; }
+        public Supplier Supplier { get; set; }
+        public string Remark { get; set; }
+        public IEnumerable<ItemPurchaseOrder> ItemPurchaseOrders { get; set; }
+        public IEnumerable<PurchaseOrder> PurchaseOrders { get; set; }
+        public IEnumerable<ItemSupplier> ItemSuppliers { get; set; }
+    }
+}
