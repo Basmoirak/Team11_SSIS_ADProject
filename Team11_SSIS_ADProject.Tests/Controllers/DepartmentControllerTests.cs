0ee73613d6bd3b98a6e2d350ae2322c30df2f41f
Callmepapa <shihaizhou1104@outlook.com>
2020-02-11 10:37:52 +0800

    test(never mind)

---
+++ Team11_SSIS_ADProject.Tests/Controllers/DepartmentControllerTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Team11_SSIS_ADProject.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Team11_SSIS_ADProject.SSIS.Contracts;
+using System.Web.Mvc;
+
+namespace Team11_SSIS_ADProject.Controllers.Tests
+{
+    [TestClass()]
+    public class DepartmentControllerTests
+    {
+        IDepartmentService departmentService;
+
+        [TestMethod()]
+        public void CreateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IndexTest()
+        {
+
+            // Arrange
+            DepartmentController controller = new DepartmentController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod()]
+        public void SaveTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ Team11_SSIS_ADProject.Tests/Controllers/RequisitionControllerTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Team11_SSIS_ADProject.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team11_SSIS_ADProject.Controllers.Tests
+{
+    [TestClass()]
+    public class RequisitionControllerTests
+    {
+        [TestMethod()]
+        public void RequisitionControllerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IndexTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SaveTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DetailsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ApproveTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RejectTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
