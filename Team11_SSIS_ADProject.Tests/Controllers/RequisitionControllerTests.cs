25efb2050b0e853de217863c7ec770b105673c64
Hein Htet Zaw <hhz18@icloud.com>
2020-02-13 18:45:17 +0800

    added requisitioncontrollerTest

--- Team11_SSIS_ADProject.Tests/Controllers/RequisitionControllerTests.cs
+++ Team11_SSIS_ADProject.Tests/Controllers/RequisitionControllerTests.cs
@@ -1,56 +1,92 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Team11_SSIS_ADProject.Controllers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+using Team11_SSIS_ADProject.SSIS.Contracts;
+using Team11_SSIS_ADProject.SSIS.Contracts.Services;
+using Team11_SSIS_ADProject.SSIS.Models;
+using Team11_SSIS_ADProject.Tests.Mocks;
+using System.Web.Mvc;
+using Team11_SSIS_ADProject.SSIS.ViewModels;
 
 namespace Team11_SSIS_ADProject.Controllers.Tests
 {
     [TestClass()]
     public class RequisitionControllerTests
     {
-        [TestMethod()]
-        public void RequisitionControllerTest()
-        {
-            Assert.Fail();
-        }
+        IRequisitionService requisitionService;
+        IItemRequisitionService itemRequisitionService;
+        IItemService itemService;
+        IDepartmentService departmentService;
+        IDisbursementService disbursementService;
+        IItemDisbursementService itemDisbursementService;
+        IInventoryService inventoryService;
 
         [TestMethod()]
         public void IndexTest()
         {
-            Assert.Fail();
-        }
+            IRepository<Requisition> requisitionContext = new MockContext<Requisition>();
 
-        [TestMethod()]
-        public void CreateTest()
-        {
-            Assert.Fail();
-        }
+            requisitionContext.Add(new Requisition());
 
+            RequisitionController requisitionController = new RequisitionController(departmentService, itemService, requisitionService, itemRequisitionService, disbursementService, itemDisbursementService, inventoryService);
+
+            var result = requisitionController.Index() as ViewResult;
+            var viewModel = (RequisitionViewModel)result.ViewData.Model;
+            // Assert
+            Assert.IsNotNull(viewModel.Requisitions);
+        }
+  
         [TestMethod()]
         public void SaveTest()
         {
-            Assert.Fail();
+     
+            Requisition requisition = new Requisition()
+            {
+                Id = "1",
+                DepartmentId = "2"
+            };
+            RequisitionController requisitionController = new RequisitionController(departmentService, itemService, requisitionService, itemRequisitionService, disbursementService, itemDisbursementService, inventoryService);
+
+            ActionResult result = requisitionController.Save(requisition);
+
+            Assert.IsNotNull(result);
         }
 
         [TestMethod()]
         public void DetailsTest()
         {
-            Assert.Fail();
+            RequisitionController requisitionController = new RequisitionController(departmentService, itemService, requisitionService, itemRequisitionService, disbursementService, itemDisbursementService, inventoryService);
+            ActionResult result = requisitionController.Details("r-1");
+
+            //ASSERT
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            ViewResult viewResult = (ViewResult)result;
         }
 
         [TestMethod()]
         public void ApproveTest()
         {
-            Assert.Fail();
+            RequisitionController requisitionController = new RequisitionController(departmentService, itemService, requisitionService, itemRequisitionService, disbursementService, itemDisbursementService, inventoryService);
+            Requisition requisition = new Requisition()
+            {
+                Id = "1",
+                Status = 0
+            };
+            requisitionController.Approve(requisition.Id);
+            Assert.AreEqual(CustomStatus.Approved, requisition.Status);
         }
 
         [TestMethod()]
         public void RejectTest()
         {
-            Assert.Fail();
+            RequisitionController requisitionController = new RequisitionController(departmentService, itemService, requisitionService, itemRequisitionService, disbursementService, itemDisbursementService, inventoryService);
+            Requisition requisition = new Requisition()
+            {
+                Id = "1",
+                Status = 0
+            };
+            requisitionController.Approve(requisition.Id);
+            Assert.AreEqual(CustomStatus.Rejected, requisition.Status);
         }
+
     }
 }
